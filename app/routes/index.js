30486304aa9b78115ece8f25f8008f7e11b3f021
Zach <trombonezach@gmail.com>
2017-01-28 17:11:32 -0500

    EJS work

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,8 +8,13 @@ const router = express.Router();
 
 // set root response
 router.get('/', (req, res) => {
-  if (req.user) { console.log(req.user); }
-  res.render('index');
+  if (!req.user) {
+    res.render('index', {state: 'authentication'});
+
+  } else {
+    console.log(req.user);
+    res.render('index', {state: 'profile'});
+  }
 });
 
 module.exports = router;
