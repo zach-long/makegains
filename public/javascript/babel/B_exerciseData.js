e1b0dac77f55c59f55e619a5a908865a3d811854
Zachary Long <zach.writes.code@gmail.com>
2017-01-31 16:17:22 -0500

    Added data visualization for exercise history based on Strength index

--- public/javascript/babel/B_exerciseData.js
+++ public/javascript/babel/B_exerciseData.js
@@ -12,32 +12,14 @@ var apiUrl = tempArray.join('/');
 get(apiUrl).then(
 response => {
   data = JSON.parse(response);
-  console.log(data);
+  displayTrend(data);
 
 }, error => {
-  console.log(error);
   data = 'An error has occured!';
-  console.log(data);
 });
 
 // Functions which can be called to display the data in a specific way
 // Each function will format data and then pass it to a method to display it
-
-//
-function displayWorkoutHistory(data) {
-  let workouts = data.exerciseHistory;
-  console.log(workouts);
-}
-
-//
-function displayDatePerformance(data, theDate) {
-  let workouts = data.exerciseHistory;
-  workouts.forEach(w => {
-    if (w.date == theDate) {
-      console.log(w);
-    }
-  });
-}
 
 /* Parses every oneRepMax entry for a single exercise
    and pulls out the highest one for each exercise session */
@@ -48,5 +30,59 @@ function displayTrend(data) {
       return datePerformance.oneRepMax;
     }).sort().pop();
   });
-  console.log(oneRepMaxArray);
+  let dateArray = workouts.map(workoutSession => {
+    let dateify = new Date(workoutSession.date);
+    let formattedDate = dateify.toLocaleDateString();
+    return formattedDate;
+  });
+  let datasetLabel = data.name;
+  let dataPointLabels = dateArray;
+  createLineChart(datasetLabel, dataPointLabels, oneRepMaxArray);
+}
+
+// creates a Chart with provided data
+function createLineChart(theExercise, dateCompletedArray, strengthIndexArray) {
+  // define context and create chart
+  let c = document.getElementById('results');
+  let context = c.getContext('2d');
+
+  let results = new Chart(context, {
+
+    type: 'line',
+
+    data: {
+      labels: dateCompletedArray,
+      datasets: [
+        {
+          label: theExercise,
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'round',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 6,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 4,
+          pointHitRadius: 10,
+          data: strengthIndexArray,
+          spanGaps: true,
+        }
+      ]
+    },
+
+    options: {
+      legend: {
+        display: false
+      }
+    }
+
+  });
 }
--- public/javascript/exerciseData.js
+++ public/javascript/exerciseData.js
@@ -13,31 +13,13 @@ var apiUrl = tempArray.join('/');
 // GET the data for this particular exercise
 get(apiUrl).then(function (response) {
   data = JSON.parse(response);
-  console.log(data);
+  displayTrend(data);
 }, function (error) {
-  console.log(error);
   data = 'An error has occured!';
-  console.log(data);
 });
 
 // Functions which can be called to display the data in a specific way
 // Each function will format data and then pass it to a method to display it
-
-//
-function displayWorkoutHistory(data) {
-  var workouts = data.exerciseHistory;
-  console.log(workouts);
-}
-
-//
-function displayDatePerformance(data, theDate) {
-  var workouts = data.exerciseHistory;
-  workouts.forEach(function (w) {
-    if (w.date == theDate) {
-      console.log(w);
-    }
-  });
-}
 
 /* Parses every oneRepMax entry for a single exercise
    and pulls out the highest one for each exercise session */
@@ -48,5 +30,57 @@ function displayTrend(data) {
       return datePerformance.oneRepMax;
     }).sort().pop();
   });
-  console.log(oneRepMaxArray);
+  var dateArray = workouts.map(function (workoutSession) {
+    var dateify = new Date(workoutSession.date);
+    var formattedDate = dateify.toLocaleDateString();
+    return formattedDate;
+  });
+  var datasetLabel = data.name;
+  var dataPointLabels = dateArray;
+  createLineChart(datasetLabel, dataPointLabels, oneRepMaxArray);
+}
+
+// creates a Chart with provided data
+function createLineChart(theExercise, dateCompletedArray, strengthIndexArray) {
+  // define context and create chart
+  var c = document.getElementById('results');
+  var context = c.getContext('2d');
+
+  var results = new Chart(context, {
+
+    type: 'line',
+
+    data: {
+      labels: dateCompletedArray,
+      datasets: [{
+        label: theExercise,
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'round',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 6,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 4,
+        pointHitRadius: 10,
+        data: strengthIndexArray,
+        spanGaps: true
+      }]
+    },
+
+    options: {
+      legend: {
+        display: false
+      }
+    }
+
+  });
 }
