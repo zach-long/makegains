30f5622668646be7f4d3bfa1d473c8987063fa6e
Zach <trombonezach@gmail.com>
2017-02-12 16:52:38 -0500

    Working on loading animation

--- public/javascript/babel/_DEV/_DEV-B_getUserAssets.js
+++ public/javascript/babel/_DEV/_DEV-B_getUserAssets.js
@@ -1,6 +1,6 @@
-var jsonExercises = [{"_id":"58a0ca0ba941c61fc52aa394","name":"Bench Press","description":"Push a barbell up while laying down in a supine position.","category":"Chest","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[{"date":"2017-02-12T20:49:26.320Z","_id":"58a0ca56a941c61fc52aa3a2","dataHistory":[{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a5"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a4"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a3"}]}],"sets":[]},{"_id":"58a0ca21a941c61fc52aa395","name":"Inline Bench Press","description":"Push a bar up while layout on your back on an incline bench.","category":"Chest","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[{"date":"2017-02-12T20:49:26.331Z","_id":"58a0ca56a941c61fc52aa3aa","dataHistory":[{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ae"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ad"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ac"},{"weight":105,"repetitions":11,"oneRepMax":143,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ab"}]}],"sets":[]},{"_id":"58a0cd59229f5c20b00293f7","name":"Bicep Curl","description":"Best exercise","category":"Arms","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[],"sets":[]}];
-var jsonWorkouts = [{"_id":"58a0ca56a941c61fc52aa39e","date":"2017-02-12T20:49:26.305Z","creator":"58a0c9dba941c61fc52aa393","__v":0,"sets":[{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa39f"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a0"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a1"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a6"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a7"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a8"},{"weight":105,"repetitions":11,"oneRepMax":143,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a9"}],"exercises":["58a0ca0ba941c61fc52aa394","58a0ca21a941c61fc52aa395"]}];
-var jsonPrograms = [{"_id":"58a0ca32a941c61fc52aa396","name":"Chest Workout","description":"Works your chest.","creator":"58a0c9dba941c61fc52aa393","__v":0,"exercises":["58a0ca0ba941c61fc52aa394","58a0ca21a941c61fc52aa395"]}];
+var jsonExercises = [];// [{"_id":"58a0ca0ba941c61fc52aa394","name":"Bench Press","description":"Push a barbell up while laying down in a supine position.","category":"Chest","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[{"date":"2017-02-12T20:49:26.320Z","_id":"58a0ca56a941c61fc52aa3a2","dataHistory":[{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a5"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a4"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a3"}]}],"sets":[]},{"_id":"58a0ca21a941c61fc52aa395","name":"Inline Bench Press","description":"Push a bar up while layout on your back on an incline bench.","category":"Chest","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[{"date":"2017-02-12T20:49:26.331Z","_id":"58a0ca56a941c61fc52aa3aa","dataHistory":[{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ae"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ad"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ac"},{"weight":105,"repetitions":11,"oneRepMax":143,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3ab"}]}],"sets":[]},{"_id":"58a0cd59229f5c20b00293f7","name":"Bicep Curl","description":"Best exercise","category":"Arms","creator":"58a0c9dba941c61fc52aa393","__v":0,"exerciseHistory":[],"sets":[]}];
+var jsonWorkouts = [];// [{"_id":"58a0ca56a941c61fc52aa39e","date":"2017-02-12T20:49:26.305Z","creator":"58a0c9dba941c61fc52aa393","__v":0,"sets":[{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa39f"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a0"},{"weight":135,"repetitions":10,"oneRepMax":180,"exercise":"Bench Press","_id":"58a0ca56a941c61fc52aa3a1"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a6"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a7"},{"weight":100,"repetitions":12,"oneRepMax":142,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a8"},{"weight":105,"repetitions":11,"oneRepMax":143,"exercise":"Inline Bench Press","_id":"58a0ca56a941c61fc52aa3a9"}],"exercises":["58a0ca0ba941c61fc52aa394","58a0ca21a941c61fc52aa395"]}];
+var jsonPrograms = [];// [{"_id":"58a0ca32a941c61fc52aa396","name":"Chest Workout","description":"Works your chest.","creator":"58a0c9dba941c61fc52aa393","__v":0,"exercises":["58a0ca0ba941c61fc52aa394","58a0ca21a941c61fc52aa395"]}];
 
 // makes an ajax request for a user's exercises, programs, or workouts
 // then displays it on the DOM in the appropriate location
@@ -11,7 +11,7 @@ function _DEV_getUserAssets(data, delimeter) {
     _DEV_displayResponse(identifiedData.data, identifiedData.type);
 
   }, err => {
-    console.log('Error in function "_DEV_handleSpecficResponseType()"');
+    console.log(`Error in function "_DEV_handleSpecficResponseType()": ${err}`);
   });
 }
 
@@ -22,7 +22,11 @@ function _DEV_handleSpecificResponseType(json) {
 
     let sampleCase = json[0];
     let response = {};
-    if (_DEV_isExercise(sampleCase)) {
+    if (sampleCase === undefined) {
+      response.data = 'You have no data for this yet!';
+      response.type = 'none';
+      resolve(response);
+    } else if (_DEV_isExercise(sampleCase)) {
       response.data = json;
       response.type = 'exercise';
       resolve(response);
@@ -38,17 +42,18 @@ function _DEV_handleSpecificResponseType(json) {
       resolve(response);
 
     } else {
-      response.data = 'You have no data for this yet!';
-      response.type = 'none';
-      resolve(response);
+      reject(Error('An error has occured, sorry this isn\'t more specific!'));
     }
-
-    reject(Error('An error has occured, sorry this isn\'t more specific!'));
   });
 }
 
 // writes the reponse to the DOM based on what type of data is received
 function _DEV_displayResponse(response, typeOfData) {
+  let loadingFields = document.getElementsByClassName('loading');
+  loadingFields.forEach(field => {
+    field.classList.add('hidden');
+  });
+
   let appendTo = undefined;
   let ul = document.createElement('ul');
   ul.classList.add('list-group');
@@ -95,15 +100,21 @@ function _DEV_displayResponse(response, typeOfData) {
               </li>`
     }).join('');
 
-  } else {
-    ul.innerHTML = `<li class="list-group-item">${response}`;
+  } else if (typeOfData == 'none') {
     if (document.getElementById('exercises').innerHTML.length < 100) {
       appendTo = document.getElementById('exercises');
+      ul.innerHTML = `<li class="list-group-item text-center">Add exercises to your account so you can add them to programs, log them in your workouts, and track your progress!</li>`;
+
     } else if (document.getElementById('workouts').innerHTML.length < 100) {
       appendTo = document.getElementById('workouts');
+      ul.innerHTML = `<li class="list-group-item text-center">You don't have any workouts logged yet! Add exercises so you can include them in logged workouts. Workouts can be free-form or follow a pre-defined program. Track your workouts so can start recording progress!</li>`;
+
     } else {
       appendTo = document.getElementById('programs');
+      ul.innerHTML = `<li class="list-group-item text-center">Have a few workouts that are "set-in-stone"? Input a your workout program so you can use it as a template while logging sets!</li>`;
     }
+  } else {
+    alert("list-group-item text-center");
   }
   appendTo.appendChild(ul);
 }
