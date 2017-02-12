658102556dc131624cd57823c311f1ba575640c5
Zachary Long <zach.writes.code@gmail.com>
2017-02-12 11:42:23 -0500

    Added local test and logic to determined type of json response

--- public/javascript/getUserAssets.js
+++ public/javascript/getUserAssets.js
@@ -1,9 +1,12 @@
-'use strict';
+"use strict";
+
+var sampleJSON = [{ "_id": "589f37a1251cc61588c4d951", "name": "Bench Press", "description": "Push a bar with your chest", "creator": "589f3792251cc61588c4d950", "__v": 0, "exerciseHistory": [{ "date": "2017-02-11T16:16:55.821Z", "_id": "589f38f7c1fbcb16052a694f", "dataHistory": [{ "weight": 150, "repetitions": 10, "oneRepMax": 200, "exercise": "Bench Press", "_id": "589f38f7c1fbcb16052a6952" }, { "weight": 155, "repetitions": 10, "oneRepMax": 206, "exercise": "Bench Press", "_id": "589f38f7c1fbcb16052a6951" }, { "weight": 155, "repetitions": 9, "oneRepMax": 201, "exercise": "Bench Press", "_id": "589f38f7c1fbcb16052a6950" }] }, { "date": "2017-02-11T16:23:21.303Z", "_id": "589f3a79b17af816f5cc8023", "dataHistory": [{ "weight": 155, "repetitions": 10, "oneRepMax": 206, "exercise": "Bench Press", "_id": "589f3a79b17af816f5cc8025" }, { "weight": 160, "repetitions": 10, "oneRepMax": 213, "exercise": "Bench Press", "_id": "589f3a79b17af816f5cc8024" }] }, { "date": "2017-02-11T16:38:56.177Z", "_id": "589f3e20c665041904fa2eb4", "dataHistory": [{ "weight": 160, "repetitions": 10, "oneRepMax": 213, "exercise": "Bench Press", "_id": "589f3e20c665041904fa2eb7" }, { "weight": 160, "repetitions": 10, "oneRepMax": 213, "exercise": "Bench Press", "_id": "589f3e20c665041904fa2eb6" }, { "weight": 160, "repetitions": 9, "oneRepMax": 207, "exercise": "Bench Press", "_id": "589f3e20c665041904fa2eb5" }] }, { "date": "2017-02-11T16:48:53.340Z", "_id": "589f4075378f8f1c13fe88cf", "dataHistory": [{ "weight": 290, "repetitions": 12, "oneRepMax": 414, "exercise": "Bench Press", "_id": "589f4075378f8f1c13fe88d0" }] }, { "date": "2017-02-11T16:58:27.658Z", "_id": "589f42b37f922f1d90f9941a", "dataHistory": [{ "weight": 123, "repetitions": 124, "exercise": "Bench Press", "_id": "589f42b37f922f1d90f9941b" }] }, { "date": "2017-02-11T17:24:26.946Z", "_id": "589f48ca7f922f1d90f99428", "dataHistory": [{ "weight": 160, "repetitions": 12, "oneRepMax": 228, "exercise": "Bench Press", "_id": "589f48ca7f922f1d90f9942b" }, { "weight": 160, "repetitions": 12, "oneRepMax": 228, "exercise": "Bench Press", "_id": "589f48ca7f922f1d90f9942a" }, { "weight": 160, "repetitions": 10, "oneRepMax": 213, "exercise": "Bench Press", "_id": "589f48ca7f922f1d90f99429" }] }], "sets": [] }, { "_id": "589f37ad251cc61588c4d952", "name": "Incline Dumbbell Press", "description": "", "creator": "589f3792251cc61588c4d950", "__v": 0, "exerciseHistory": [{ "date": "2017-02-11T16:16:55.837Z", "_id": "589f38f7c1fbcb16052a6955", "dataHistory": [{ "weight": 50, "repetitions": 10, "oneRepMax": 66, "exercise": "Incline Dumbbell Press", "_id": "589f38f7c1fbcb16052a6958" }, { "weight": 50, "repetitions": 10, "oneRepMax": 66, "exercise": "Incline Dumbbell Press", "_id": "589f38f7c1fbcb16052a6957" }, { "weight": 50, "repetitions": 10, "oneRepMax": 66, "exercise": "Incline Dumbbell Press", "_id": "589f38f7c1fbcb16052a6956" }] }, { "date": "2017-02-11T16:23:21.315Z", "_id": "589f3a79b17af816f5cc8027", "dataHistory": [{ "weight": 55, "repetitions": 12, "oneRepMax": 78, "exercise": "Incline Dumbbell Press", "_id": "589f3a79b17af816f5cc8028" }] }, { "date": "2017-02-11T16:38:56.193Z", "_id": "589f3e20c665041904fa2eba", "dataHistory": [{ "weight": 55, "repetitions": 12, "oneRepMax": 78, "exercise": "Incline Dumbbell Press", "_id": "589f3e20c665041904fa2ebc" }, { "weight": 55, "repetitions": 12, "oneRepMax": 78, "exercise": "Incline Dumbbell Press", "_id": "589f3e20c665041904fa2ebb" }] }, { "date": "2017-02-11T17:24:26.959Z", "_id": "589f48ca7f922f1d90f9942f", "dataHistory": [{ "weight": 60, "repetitions": 12, "oneRepMax": 85, "exercise": "Incline Dumbbell Press", "_id": "589f48ca7f922f1d90f99432" }, { "weight": 60, "repetitions": 12, "oneRepMax": 85, "exercise": "Incline Dumbbell Press", "_id": "589f48ca7f922f1d90f99431" }, { "weight": 60, "repetitions": 12, "oneRepMax": 85, "exercise": "Incline Dumbbell Press", "_id": "589f48ca7f922f1d90f99430" }] }], "sets": [] }, { "_id": "589f37b6251cc61588c4d953", "name": "Chest Fly (Machine)", "description": "", "creator": "589f3792251cc61588c4d950", "__v": 0, "exerciseHistory": [{ "date": "2017-02-11T16:16:55.857Z", "_id": "589f38f7c1fbcb16052a695c", "dataHistory": [{ "weight": 120, "repetitions": 12, "oneRepMax": 171, "exercise": "Chest Fly (Machine)", "_id": "589f38f7c1fbcb16052a695f" }, { "weight": 120, "repetitions": 12, "oneRepMax": 171, "exercise": "Chest Fly (Machine)", "_id": "589f38f7c1fbcb16052a695e" }, { "weight": 120, "repetitions": 12, "oneRepMax": 171, "exercise": "Chest Fly (Machine)", "_id": "589f38f7c1fbcb16052a695d" }] }, { "date": "2017-02-11T16:38:56.220Z", "_id": "589f3e20c665041904fa2ec0", "dataHistory": [{ "weight": 125, "repetitions": 12, "oneRepMax": 178, "exercise": "Chest Fly (Machine)", "_id": "589f3e20c665041904fa2ec3" }, { "weight": 155, "repetitions": 12, "oneRepMax": 221, "exercise": "Chest Fly (Machine)", "_id": "589f3e20c665041904fa2ec2" }, { "weight": 155, "repetitions": 12, "oneRepMax": 221, "exercise": "Chest Fly (Machine)", "_id": "589f3e20c665041904fa2ec1" }] }, { "date": "2017-02-11T17:24:26.988Z", "_id": "589f48ca7f922f1d90f99435", "dataHistory": [{ "weight": 120, "repetitions": 15, "exercise": "Chest Fly (Machine)", "_id": "589f48ca7f922f1d90f99437" }, { "weight": 120, "repetitions": 14, "exercise": "Chest Fly (Machine)", "_id": "589f48ca7f922f1d90f99436" }] }], "sets": [] }, { "_id": "589fa43e8a47ef0ca0ea92aa", "name": "Bicep Curl", "description": "Curl", "category": "Arms", "creator": "589f3792251cc61588c4d950", "__v": 0, "exerciseHistory": [], "sets": [] }, { "_id": "58a084e56af5c40cbf2d7af4", "name": "Crunch", "description": "", "category": "Core", "creator": "589f3792251cc61588c4d950", "__v": 0, "exerciseHistory": [], "sets": [] }];
 
 // makes an ajax request for a user's exercises, programs, or workouts
 // then displays it on the DOM in the appropriate location
+/*
 function getUserAssets(url, delimeter) {
-  var path = void 0;
+  let path;
 
   if (delimeter !== null) {
     path = url + '/' + delimeter;
@@ -11,28 +14,76 @@ function getUserAssets(url, delimeter) {
     path = url;
   }
 
-  get(path).then(function (response) {
-    var data = JSON.parse(response);
+  get(path)
+  .then(response => {
+    let data = JSON.parse(response);
 
-    handleSpecficResponseType(data).then(function (response) {
-      displayResponse(typeOfData);
-    }, function (err) {
+    handleSpecificResponseType(data)
+    .then(formattedResponse => {
+      displayResponse(typeOfData, formattedResponse);
+
+    }, err => {
       console.log('Error in function "handleSpecficResponseType()"');
     });
-  }, function (err) {
+
+  }, err => {
     console.log('Error retrieving data in "getUserAssets()"');
+  });
+}
+*/
+function getUserAssetsTest(json, delimeter) {
+
+  handleSpecificResponseType(json).then(function (data) {
+    displayResponse(data.data, data.type);
+  }, function (error) {
+    console.log('Error in function "handleSpecficResponseType()" - ' + error);
   });
 }
 
 // determines whether the response was an exercise or a program or a workout
 // then returns a formatted version of the data
-function handleSpecificResponseType(data) {
-  return new Promise(function (resolve, reject) {});
+function handleSpecificResponseType(json) {
+  return new Promise(function (resolve, reject) {
+
+    var sampleCase = json[0];
+    var response = { data: json };
+    if (isExercise(sampleCase)) {
+      response.type = 'exercise';
+    } else if (isWorkout(sampleCase)) {
+      response.type = 'workout';
+    } else if (isProgram(sampleCase)) {
+      response.type = 'program';
+    } else {
+      var errorMessage = 'An error has occured parsing the server response';
+    }
+
+    errorMessage === undefined ? resolve(response) : reject(Error(errorMessage));
+  });
 }
 
 // writes the reponse to the DOM based on what type of data is received
-function displayResponse(typeOfData) {}
+function displayResponse(formattedResponse, typeOfData) {
+  console.log('Data is of type: ' + typeOfData);
+  console.log(formattedResponse);
+}
 
-getUserAssets('/user/exercises', null);
-getUserAssets('/user/programs', null);
-getUserAssets('/user/workouts', null);
+// functions to check the type of the json response
+function isExercise(sample) {
+  return sample.hasOwnProperty('exerciseHistory') ? true : false;
+}
+function isWorkout(sample) {
+  return sample.hasOwnProperty('exercises') ? true : false;
+}
+function isProgram(sample) {
+  if (!isExercise(sample) && !isWorkout(sample)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+getUserAssetsTest(sampleJSON, null);
+
+//getUserAssets('localhost:3000/user/exercises', null);
+//getUserAssets('localhost:3000/user/programs', null);
+//getUserAssets('localhost:3000/user/workouts', null);
