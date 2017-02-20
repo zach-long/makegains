0d6da546032baa073fdab15830b4bf6f40047a53
Zachary Long <zach.writes.code@gmail.com>
2017-02-20 12:59:47 -0500

    Protected Exercise routes

--- app/resources/helperFunctions.js
+++ app/resources/helperFunctions.js
@@ -4,6 +4,34 @@ const Exercise = require('../models/exercise.js');
 const Program = require('../models/program.js');
 const Workout = require('../models/workout.js');
 
+module.exports.userHasResource = function userHasResource(user, resource) {
+  return new Promise((resolve, reject) => {
+    if (user === undefined) {
+      resolve(false);
+    }
+
+    user.exercises.forEach((exercise) => {
+      if (exercise == resource) {
+        resolve(true);
+      }
+    });
+
+    user.workouts.forEach((workout) => {
+      if (workout == resource) {
+        resolve(true);
+      }
+    });
+
+    user.programs.forEach((program) => {
+      if (program == resource) {
+        resolve(true);
+      }
+    });
+
+    resolve(false);
+  });
+}
+
 module.exports.getPerformedSets = function getPerformedSets(user) {
   return new Promise((resolve, reject) => {
     let doneCount = 0;
--- app/routes/exerciseRoutes.js
+++ app/routes/exerciseRoutes.js
@@ -9,6 +9,9 @@ const router = express.Router();
 // import one rep max calcuator
 const calculateEstimatedOneRepMax = require('../resources/oneRepMax.js');
 
+// import helper function
+const userHasResource = require('../resources/helperFunctions.js').userHasResource;
+
 // import models
 const User = require('../models/user.js');
 const Exercise = require('../models/exercise.js');
@@ -16,10 +19,16 @@ const Program = require('../models/program.js');
 
 // GET request to return all of a users exercises
 router.get('/myexercises', (req, res) => {
-  Exercise.getOwnExercises(req.user, (err, exercises) => {
-    if (err) throw err
-    res.json(exercises);
-  });
+  if (req.user) {
+    Exercise.getOwnExercises(req.user, (err, exercises) => {
+      if (err) throw err
+
+      res.json(exercises);
+    });
+
+  } else {
+    res.json({ error: 'You must be logged in to view exercises' });
+  }
 });
 
 // GET request to display page where User can create an exercise
@@ -37,12 +46,20 @@ router.get('/new', (req, res) => {
 // GET request to display specific information about an exercise
 router.get('/detail/:id', (req, res) => {
   if (req.user) {
-    Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
-      if (err) throw err;
 
-      res.render('exerciseDetail', {exercise: exercise});
+    userHasResource(req.user, req.params.id).then((bool) => {
+      if (bool) {
+
+        Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
+          if (err) throw err;
+
+          res.render('exerciseDetail', {exercise: exercise});
+        });
+
+      } else {
+        res.redirect('/user');
+      }
     });
-
   } else {
     res.redirect('/');
   }
@@ -50,26 +67,40 @@ router.get('/detail/:id', (req, res) => {
 
 // POST request to add set info to an exercise
 router.post('/set/add', (req, res) => {
-  // get the exercise
-  Exercise.getExerciseByExerciseId(req.body.exercise, (err, exercise) => {
-    // calculate estimated one rep max
-    let estMax = calculateEstimatedOneRepMax(req.body.weight, req.body.reps);
+  // check if User is logged in
+  if (req.user) {
+    // ensure User owns the Exercise they are adding a set to
+    userHasResource(req.user, req.body.exercise).then((bool) => {
+      if (bool) {
 
-    // define the new set
-    let newSet = {
-      weight: req.body.weight,
-      repetitions: req.body.reps,
-      oneRepMax: estMax,
-      exercise: exercise.name
-    }
+        // get the exercise
+        Exercise.getExerciseByExerciseId(req.body.exercise, (err, exercise) => {
+          // calculate estimated one rep max
+          let estMax = calculateEstimatedOneRepMax(req.body.weight, req.body.reps);
 
-    // update exercise
-    Exercise.addSet(exercise, newSet, (err, result) => {
-      if (err) throw err;
+          // define the new set
+          let newSet = {
+            weight: req.body.weight,
+            repetitions: req.body.reps,
+            oneRepMax: estMax,
+            exercise: exercise.name
+          }
 
-      res.redirect('/program/log');
+          // update exercise
+          Exercise.addSet(exercise, newSet, (err, result) => {
+            if (err) throw err;
+
+            res.redirect('/program/log');
+          });
+        });
+
+      } else {
+        res.redirect('/user');
+      }
     });
-  });
+  } else {
+    res.redirect('/');
+  }
 });
 
 // POST request to creating a new exercise
@@ -109,61 +140,90 @@ router.post('/new', (req, res) => {
 
 // POST request to delete a set from exercise.sets
 router.post('/set/delete/:exerciseId/:setId', (req, res) => {
-  // get the exercise to remove a set from
-  Exercise.getExerciseByExerciseId(req.params.exerciseId, (err, exercise) => {
+  userHasResource(req.user, req.params.exerciseId).then((bool) => {
+    if (bool) {
+      // get the exercise to remove a set from
+      Exercise.getExerciseByExerciseId(req.params.exerciseId, (err, exercise) => {
 
-    // find the set requested for removal
-    Exercise.removeSet(exercise, req.params.setId, (err, updatedExercise) => {
-      if (err) throw err;
+        // find the set requested for removal
+        Exercise.removeSet(exercise, req.params.setId, (err, updatedExercise) => {
+          if (err) throw err;
 
-      req.flash('success', 'Set deleted');
-      res.redirect('/program/log');
-    });
+          req.flash('success', 'Set deleted');
+          res.redirect('/program/log');
+        });
+      });
+
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
 // GET request to update an exercise
 router.get('/edit/:id', (req, res) => {
-  Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
-    if (err) throw err;
+  userHasResource(req.user, req.params.id).then((bool) => {
+    if (bool) {
+      Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
+        if (err) throw err;
 
-    res.render('editExercise', {exercise: exercise});
+        res.render('editExercise', {exercise: exercise});
+      });
+
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
 // POST request to update an exercise
 router.post('/edit/:id', (req, res) => {
-  // define new exercise information
-  let newParams = {
-    name: req.body.name,
-    description: req.body.description,
-    category: req.body.category
-  }
+  userHasResource(req.user, req.params.id).then((bool) => {
+    if (bool) {
+      // define new exercise information
+      let newParams = {
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category
+      }
 
-  // update the exercise with requested information
-  Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
-    if (err) throw err;
+      // update the exercise with requested information
+      Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
+        if (err) throw err;
 
-    Exercise.updateExercise(exercise, newParams, (err, result) => {
-      if (err) throw err;
+        Exercise.updateExercise(exercise, newParams, (err, result) => {
+          if (err) throw err;
 
-      req.flash('success', 'Exercise "' + exercise.name + '" updated!');
-      res.redirect('/user');
-    });
+          req.flash('success', 'Exercise "' + exercise.name + '" updated!');
+          res.redirect('/user');
+        });
+      });
+
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
 // DELETE request to delete an exercise
 router.post('/delete/:id', (req, res) => {
-  Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
-    if (err) throw err;
+  userHasResource(req.user, req.params.id).then((bool) => {
+    if (bool) {
+      
+      Exercise.getExerciseByExerciseId(req.params.id, (err, exercise) => {
+        if (err) throw err;
 
-    let eName = exercise.name;
-    Exercise.deleteExercise(exercise, (err, result) => {
-      if (err) throw err;
+        let eName = exercise.name;
+        Exercise.deleteExercise(exercise, (err, result) => {
+          if (err) throw err;
 
-      req.flash('success', `Exercise '${eName}' deleted successfully!`);
-    });
+          req.flash('success', `Exercise '${eName}' deleted successfully!`);
+        });
+      });
+
+    } else {
+      res.redirct('/');
+    }
   });
 });
 
