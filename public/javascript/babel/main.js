71dca3f69d56ad5e0b6c628e31c874e48b47506d
Zachary Long <zach.writes.code@gmail.com>
2017-02-21 21:06:59 -0500

    Setup for category sorting addition

--- public/javascript/babel/main.js
+++ public/javascript/babel/main.js
@@ -1,16 +1,21 @@
 import { getUserAssets } from './modules/getUserAssets.js';
 import { displayExerciseHistory } from './modules/exerciseData.js';
-import { sort } from './modules/sort.js';
+import { setSortingListeners } from './modules/sort.js';
 import { isProfilePage, isExerciseDetailPage } from './modules/helperFunctions.js';
 
 let thisPath = window.location.pathname;
 
 if (isProfilePage(thisPath)) {
+  // populate with initial user data
   getUserAssets('https://makegains.herokuapp.com/user/exercises', null);
   getUserAssets('https://makegains.herokuapp.com/user/programs', null);
   getUserAssets('https://makegains.herokuapp.com/user/workouts', null);
+
+  // set event listeners to sort categories
+  setSortingListeners();
 }
 
 if (isExerciseDetailPage(thisPath)) {
+  // display a chart with exercise data over time
   displayExerciseHistory();
 }
---
+++ public/javascript/babel/modules/sort.js
@@ -0,0 +1,7 @@
+import { getUserAssets } from './getUserAssets.js';
+
+function setSortingListeners() {
+
+}
+
+export { setSortingListeners };
